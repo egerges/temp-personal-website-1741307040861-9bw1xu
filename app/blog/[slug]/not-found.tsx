007d021b5eeb241bc3882d2700f9c6c9bcdecc9e
Elio Gerges <91638583+egerges@users.noreply.github.com>
2025-03-06 16:24:20 -0800

    Copy app/blog/[slug]/not-found.tsx from personal-website

---
+++ app/blog/[slug]/not-found.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from "react";
+
+export default function NotFound() {
+    return (
+      <section className="py-20 text-center">
+        <p className="text-gray-500">Sorry, that post could not be found.</p>
+      </section>
+    );
+  }
+  
